c802c66254a7fe160ac7972b57b229146721b2f6
Arafat77mo <mohammed.poop444@gmail.com>
2022-07-12 21:38:02 +0200

    delete  Book controller

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +21,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('Book','App\Http\Controllers\BookController')->middleware('auth');
