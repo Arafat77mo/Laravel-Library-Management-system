9f5ae29160005e5483523a181bd6fbc5496154d4
Arafat77mo <mohammed.poop444@gmail.com>
2022-07-12 20:51:53 +0200

    Book controller

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('Book','App\Http\Controllers\BookController')->middleware('auth');
