71ca7aa1f68a0d9bb6319e725de6d908ad69612b
unknown <alhousany1241997@gmail.com>
2022-07-14 17:28:59 +0200

    add wishlist

---
+++ laravel/database/migrations/2022_07_13_205657_create_wish_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWishListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wish_lists', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('book_id')->unsigned();
+            $table->primary(['user_id','book_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wish_lists');
+    }
+}
