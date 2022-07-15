d6a6f3dbd3239aecfc8bc4aade96cf34164b5507
islamhassaan <is2015ha@gmail.com>
2022-07-15 22:04:24 +0200

    search

--- laravel/resources/views/user/layout.blade.php
+++ laravel/resources/views/user/layout.blade.php
@@ -100,7 +100,7 @@
 
 
 
-        {{-- order by section start --}}
+        <!-- {{-- order by section start --}}
 
 
             {{-- <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-4 me-5"   >
@@ -182,7 +182,7 @@
                       </div>
                     </div>
               </div> --}}
-          {{-- order by section end --}}
+          {{-- order by section end --}} -->
 
 
 
@@ -204,6 +204,7 @@
     </ul>
   </div>
 
+  <a href="{{Route('search')}}"><button type="submit" class="btn btn-primary">Search</button></a> 
 
     <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
       latest
@@ -219,6 +220,13 @@
   </div>
     </div>
 
+
+    <!-- search -->
+
+    
+            
+       
+    
         <main class="py-4 row  ">
 
         <div class="col-3 m-0 h-1000 bg-primary">
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\searchController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -21,6 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [BookController::class, 'index'])->name('home');
+Route::get('/search', [searchController::class, 'search'])->name('search')->middleware('auth');
+Route::post('/search', [searchController::class, 'getSearch'])->name('getSearch');
 
 
 
